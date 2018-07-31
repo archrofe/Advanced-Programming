ac5064967410fa4c5aefc23137491c25460f4389
archrofe <archrofe@gmail.com>
2018-07-31 11:59:48 +1000

    Hitboxes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Include Artificial Intelligence part of API
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private int health = 100;
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+    }
+
+    public NavMeshAgent agent;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update the AI's target position
+        agent.SetDestination(target.position);
+    }
+
+    public void DealDamage(int damageDealt)
+    {
+        health -= damageDealt;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CTRL + SHIFT + M (Method)
+public class Player : MonoBehaviour
+{
+    public BoxCollider hitBox;
+    public int damage = 50;
+    // Attribute
+    [Tooltip("Duration hitbox is enabled (in seconds)")]
+    public float hitDuration = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Run hit sequence
+            StartCoroutine(Hit());
+        }
+    }
+
+    IEnumerator Hit()
+    {
+        hitBox.enabled = true;
+        yield return new WaitForSeconds(hitDuration);
+        hitBox.enabled = false;
+    }
+
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        // Detect enemy
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (true)
+        {
+            // Deal damage
+            enemy.DealDamage(damage);
+        }
+    }
+}
